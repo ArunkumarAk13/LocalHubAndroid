b207483cd79d8fb0bb106e7db4afd15498bb5fcb
Arun Kumar S <93981548+ArunkumarAk13@users.noreply.github.com>
2025-05-14 23:30:27 +0530

    web

--- android/app/src/main/java/in/co/buhlacol/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package in.co.buhlacol;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {
-    // This activity is the main entry point for your app
-}
